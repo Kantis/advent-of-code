77b1441fe95cb13a8064995c6700734330a778d9
Emil Kantis <emil.kantis@protonmail.com>
2021-12-02 23:17:00 +0100

    prevent overwriting ongoing work

--- fetchInput.sh
+++ fetchInput.sh
@@ -5,10 +5,14 @@ DAY=$1
 PADDED_DAY=$(printf "%02d" $DAY)
 BASE_DIR=src/test/resources
 
-mkdir -p $BASE_DIR/day$PADDED_DAY
-# touch $BASE_DIR/day$PADDED_DAY/sample.txt
-# curl -v https://adventofcode.com/2021/day/$DAY/input --cookie "session=$SESSION" > $BASE_DIR/day$PADDED_DAY/input.txt
+if [ ! -d  $BASE_DIR/day$PADDED_DAY ]; then
+  mkdir -p $BASE_DIR/day$PADDED_DAY
+  touch $BASE_DIR/day$PADDED_DAY/sample.txt
+  curl -v https://adventofcode.com/2021/day/$DAY/input --cookie "session=$SESSION" > $BASE_DIR/day$PADDED_DAY/input.txt
 
-
-cat src/test/resources/DayTemplate.ktt | sed -e "s/\$PADDED_DAY/$PADDED_DAY/g" | sed -e "s/\$DAY/$DAY/g" > src/main/kotlin/day${PADDED_DAY}.kt
-cat src/test/resources/DayTestTemplate.ktt | sed -e "s/\$PADDED_DAY/$PADDED_DAY/g" | sed -e "s/\$DAY/$DAY/g" > src/test/kotlin/Day${PADDED_DAY}Test.kt
+  cat src/test/resources/DayTemplate.ktt | sed -e "s/\$PADDED_DAY/$PADDED_DAY/g" | sed -e "s/\$DAY/$DAY/g" > src/main/kotlin/day${PADDED_DAY}.kt
+  cat src/test/resources/DayTestTemplate.ktt | sed -e "s/\$PADDED_DAY/$PADDED_DAY/g" | sed -e "s/\$DAY/$DAY/g" > src/test/kotlin/Day${PADDED_DAY}Test.kt
+else
+  echo "$BASE_DIR/day$PADDED_DAY already exists"
+  exit 1
+fi
