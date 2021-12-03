092a5a121b52583e6683fcccc6d441d46a3d0170
Emil Kantis <emil.kantis@protonmail.com>
2021-12-03 09:38:07 +0100

    day1

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -1,12 +1,14 @@
 object Day1 {
 
-    fun part1(lines: Sequence<String>): Int {
-        return 1
-    }
+    fun part1(lines: Sequence<String>): Int =
+        lines.map { it.toInt() }
+            .windowed(2)
+            .count { it[1] > it[0] }
 
-    fun part2(lines: Sequence<String>): Int {
-        return 2
-    }
+    fun part2(lines: Sequence<String>): Int =
+        lines.map { it.toInt() }
+            .windowed(4)
+            .count { it.slice(1..3).sum() > it.slice(0..2).sum() }
 }
 
 fun main() {
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -5,18 +5,18 @@ class Day01Test : FunSpec(
     {
         test("sample") {
             val sample = loadAsText("/day01/sample.txt")
-            Day1.part1(sample.lineSequence()) shouldBe 1
+            Day1.part1(sample.lineSequence()) shouldBe 7
         }
 
         test("sample2") {
             val sample = loadAsText("/day01/sample.txt")
-            Day1.part2(sample.lineSequence()) shouldBe 2
+            Day1.part2(sample.lineSequence()) shouldBe 5
         }
 
         test("test input") {
             val sample = loadAsText("/day01/input.txt")
-            Day1.part1(sample.lineSequence()) shouldBe 1
-            Day1.part2(sample.lineSequence()) shouldBe 2
+            Day1.part1(sample.lineSequence()) shouldBe 1529
+            Day1.part2(sample.lineSequence()) shouldBe 1567
         }
     }
 )
