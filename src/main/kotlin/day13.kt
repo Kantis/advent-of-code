70b9002d03977133a1381f75eff82d36ec95f609
Emil Kantis <emil.kantis@protonmail.com>
2021-12-13 09:17:59 +0100

    cleaning

---
+++ src/main/kotlin/day13.kt
@@ -0,0 +1,54 @@
+object day13 {
+    private data class Point(val x: Int, val y: Int)
+
+    private fun Set<Point>.foldX(f: Int) =
+        map { (x, y) -> Point(if (x >= f) 2 * f - x else x, y) }.toSet()
+
+    private fun Set<Point>.foldY(f: Int) =
+        map { (x, y) -> Point(x, if (y >= f) 2 * f - y else y) }.toSet()
+
+    private fun interface PaperFold {
+        operator fun invoke(points: Set<Point>): Set<Point>
+    }
+
+    private fun compose(f1: PaperFold, f2: PaperFold) =
+        PaperFold { points -> f2(f1(points)) }
+
+    private fun parseInput(lines: String): Pair<Set<Point>, List<PaperFold>> {
+        val (pointInfo, foldInstructions) = lines.split("\n\n")
+        val points = pointInfo.lines().map {
+            it.split(",").let { (x, y) ->
+                Point(x.toInt(), y.toInt())
+            }
+        }.toSet()
+
+        val folds = foldInstructions.lines().map {
+            it.split("=").let { (axis, pos) ->
+                if (axis.endsWith("x")) PaperFold { points -> points.foldX(pos.toInt()) }
+                else PaperFold { points -> points.foldY(pos.toInt()) }
+            }
+        }
+        return Pair(points, folds)
+    }
+
+    fun part1(lines: String): Int {
+        val (points, folds) = parseInput(lines)
+
+        return folds[0](points).count()
+    }
+
+    fun part2(lines: String) {
+        val (points, folds) = parseInput(lines)
+        val f = folds.reduce(::compose)
+        val folded = f(points)
+
+        for (y in 0..100) {
+            for (x in 0..100) {
+                if (folded.contains(Point(x, y))) print('#') else print('.')
+            }
+            println()
+        }
+    }
+}
+
+fun main() {}
--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -5,18 +5,18 @@ class Day13Test : FunSpec(
     {
         test("sample") {
             val sample = loadAsText("/day13/sample.txt")
-            Day13.part1(sample) shouldBe 17
+            day13.part1(sample) shouldBe 17
         }
 
         test("sample2") {
             val sample = loadAsText("/day13/sample.txt")
-            Day13.part2(sample)
+            day13.part2(sample)
         }
 
         test("test input") {
             val sample = loadAsText("/day13/input.txt")
-            Day13.part1(sample) shouldBe 655
-            Day13.part2(sample)
+            day13.part1(sample) shouldBe 655
+            day13.part2(sample)
         }
     }
 )
